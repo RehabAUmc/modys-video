4cf17df67fd867d5f8bf81db28059308fbe56960
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2021-09-20 10:38:58 +0200

    Add empty rows in case video is not long enough

--- tests/test_data_generators.py
+++ tests/test_data_generators.py
@@ -20,9 +20,10 @@ def test_data_generators():
     scores_df = read_scores()
     scores_df = MultipleScoreSelector().transform(scores_df)
     test_generator = RawDataGenerator(scores_df, batch_size=2)
-    X, y = test_generator.__getitem__(0)
-    assert X.shape == (2, 501, 42)
-    assert y.shape == (2, 1)
+    for i in range(len(test_generator)):
+        X, y = test_generator.__getitem__(i)
+        assert X.shape == (2, 501, 42)
+        assert y.shape == (2, 1)
 
 
 def test_engineered_feautures_data_generator():
@@ -31,6 +32,7 @@ def test_engineered_feautures_data_generator():
     feature_conf = FeatureConfiguration(dmtp_bodyparts=['ankle', 'hip'],
                                         dtl_bodyparts=['ankle', 'knee'])
     test_generator = EngineeredFeaturesDataGenerator(scores_df, feature_conf, batch_size=2)
-    X, y = test_generator.__getitem__(0)
-    assert X.shape == (2, 40)  # 2 samples in batch, 4 features with 10 bins
-    assert y.shape == (2, 1)
+    for i in range(len(test_generator)):
+        X, y = test_generator.__getitem__(i)
+        assert X.shape == (2, 40)  # 2 samples in batch, 4 features with 10 bins
+        assert y.shape == (2, 1)

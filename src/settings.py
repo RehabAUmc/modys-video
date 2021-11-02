6ce27035ef16c3514671b0f5d860cf5495089a38
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2021-11-02 12:56:04 +0100

    First draft for experiment 3

--- src/settings.py
+++ src/settings.py
@@ -4,3 +4,4 @@ PROJECT_ROOT_FOLDER = Path(__file__).parent.parent
 DATA_FOLDER = PROJECT_ROOT_FOLDER / 'data'
 SCORES_DATA_FILEPATH = DATA_FOLDER / 'data_Scoring_DIS_proximal_trunk_V1.1.xlsx'
 LYING_VIDEOS_DATA_FOLDER = DATA_FOLDER / 'data_lying_052929'
+SITTING_VIDEOS_DATA_FOLDER = DATA_FOLDER / 'data_sitting_052020'

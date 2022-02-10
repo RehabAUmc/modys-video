31c4c4076f3270d5a0ddb9194ad2dec8be37ba6c
Shankara <shankaravandeven@hotmail.com>
2022-02-10 17:34:37 +0100

    Lines and points visualisation

--- src/plotting.py
+++ src/plotting.py
@@ -1,56 +1,76 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-def newline(p1, p2):
-    ''' Plot a line. '''
+BODYPART_LINES = [('ankle1', 'knee1', 'ro-'), 
+                  ('knee1', 'hip1', 'go-'),
+                  ('hip1', 'shoulder1', 'yo-'),
+                  ('shoulder1', 'elbow1', 'co-'),
+                  ('elbow1', 'wrist1', 'mo-'),
+                  ('ankle2', 'knee2', 'ro-'), 
+                  ('knee2', 'hip2', 'go-'),
+                  ('hip2', 'shoulder2', 'yo-'),
+                  ('shoulder2', 'elbow2', 'co-'),
+                  ('elbow2', 'wrist2', 'mo-'),
+                  ('hip1', 'hip2', 'yo-'),
+                  ('shoulder1', 'shoulder2', 'co-')]
+DEFAULT_BODYPARTS = ['ankle1', 'knee1', 'hip1', 'shoulder1', 'elbow1', 'wrist1', 'ankle2', 'knee2', 'hip2', 'shoulder2', 'elbow2', 'wrist2']
+COLORS = ['ro-', 'go-', 'yo-', 'co-', 'mo-', 'bo-']
+
+def continues_line(p1, p2):
+    ''' Plot a continues line. '''
     ax = plt.gca()
     xmin, xmax = ax.get_xbound()
-
     if(p2[0] == p1[0]):
         xmin = xmax = p1[0]
         ymin, ymax = ax.get_ybound()
     else:
         ymax = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmax-p1[0])
         ymin = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmin-p1[0])
-
     l = mlines.Line2D([xmin,xmax], [ymin,ymax])
     ax.add_line(l)
-    return l
-        
-def plot_body_lines(ankle_right_x, ankle_right_y, knee_right_x, knee_right_y, hip_right_x, hip_right_y, 
-            wrist_right_x, wrist_right_y, elbow_right_x, elbow_right_y, shoulder_right_x, shoulder_right_y, 
-            ankle_left_x, ankle_left_y, knee_left_x, knee_left_y, hip_left_x, hip_left_y, 
-            wrist_left_x, wrist_left_y, elbow_left_x, elbow_left_y, shoulder_left_x, shoulder_left_y):
-    ''' Plot all body limbs. '''
-    plt.plot([ankle_right_x, knee_right_x], [ankle_right_y, knee_right_y], 'ro-')
-    plt.plot([knee_right_x, hip_right_x], [knee_right_y, hip_right_y], 'go-')
-    plt.plot([hip_right_x, shoulder_right_x], [hip_right_y, shoulder_right_y], 'yo-')
-    plt.plot([shoulder_right_x, elbow_right_x], [shoulder_right_y, elbow_right_y], 'co-')
-    plt.plot([elbow_right_x, wrist_right_x], [elbow_right_y, wrist_right_y], 'mo-')
-
-    plt.plot([ankle_left_x, knee_left_x], [ankle_left_y, knee_left_y], 'ro-')
-    plt.plot([knee_left_x, hip_left_x], [knee_left_y, hip_left_y], 'go-')
-    plt.plot([hip_left_x, shoulder_left_x], [hip_left_y, shoulder_left_y], 'yo-')
-    plt.plot([shoulder_left_x, elbow_left_x], [shoulder_left_y, elbow_left_y], 'co-')
-    plt.plot([elbow_left_x, wrist_left_x], [elbow_left_y, wrist_left_y], 'mo-')
-
-    plt.plot([hip_right_x, hip_left_x], [hip_right_y, hip_left_y], 'bo-')
-    plt.plot([shoulder_right_x, shoulder_left_x], [shoulder_right_y, shoulder_left_y], 'bo-')
     
-    newline([shoulder_right_x, shoulder_right_y],[hip_right_x, hip_right_y])
-    newline([shoulder_left_x, shoulder_left_y],[hip_left_x, hip_left_y])
+def plot_continues_line(df, bodypart1, bodypart2, frame=None):
+    if frame == None:
+        x1 = df[bodypart1]['x'].mean()
+        y1 = df[bodypart1]['y'].mean()
+        x2 = df[bodypart2]['x'].mean()
+        y2 = df[bodypart2]['y'].mean()
+    else:
+        x1 = df[bodypart1]['x'][frame]
+        y1 = df[bodypart1]['y'][frame]
+        x2 = df[bodypart2]['x'][frame]
+        y2 = df[bodypart2]['y'][frame]
+    continues_line((x1, y1), (x2, y2))
+
+def plot_bodypart_lines(df, frame = None, bodypart_lines = BODYPART_LINES):
+    for line in bodypart_lines:
+        if frame == None:
+            x1 = df[line[0]]['x'].mean()
+            y1 = df[line[0]]['y'].mean()
+            x2 = df[line[1]]['x'].mean()
+            y2 = df[line[1]]['y'].mean()
+        else:
+            x1 = df[line[0]]['x'][frame]
+            y1 = df[line[0]]['y'][frame]
+            x2 = df[line[1]]['x'][frame]
+            y2 = df[line[1]]['y'][frame]
+        plt.plot([x1, x2], [y1, y2], line[2])
+    
+def plot_bodypoint(df, bodypart, color, alpha = 0.1, frame = None):
+    if frame == None:
+        x = df[bodypart]['x'].mean()
+        y = df[bodypart]['y'].mean()
+    else:
+        x = df[bodypart]['x'][frame]
+        y = df[bodypart]['y'][frame]
+    plt.plot(x, y, color, alpha=alpha)
 
 def stick_movie(df):
     ''' Create a stick figure movie for the XY data. '''
     df = -1*df
     fig = plt.figure(figsize=(10,12))
-    for i in range(len(df)):
-        plt.xlim(-15,-2)   
-        plt.ylim(-15,-0) 
-        plot_body_lines(df.iloc[i]['ankle1'], df.iloc[i]['ankle1.1'], df.iloc[i]['knee1'], df.iloc[i]['knee1.1'], df.iloc[i]['hip1'], df.iloc[i]['hip1.1'],
-                    df.iloc[i]['wrist1'], df.iloc[i]['wrist1.1'], df.iloc[i]['elbow1'], df.iloc[i]['elbow1.1'], df.iloc[i]['shoulder1'], df.iloc[i]['shoulder1.1'], 
-                    df.iloc[i]['ankle2'], df.iloc[i]['ankle2.1'], df.iloc[i]['knee2'], df.iloc[i]['knee2.1'], df.iloc[i]['hip2'], df.iloc[i]['hip2.1'],
-                    df.iloc[i]['wrist2'], df.iloc[i]['wrist2.1'], df.iloc[i]['elbow2'], df.iloc[i]['elbow2.1'], df.iloc[i]['shoulder2'], df.iloc[i]['shoulder2.1'])
+    for i in range(len(df)): 
+        plot_bodypart_lines(df, frame=i)
         fig.canvas.draw()
         plt.pause(0.0000000001)
         plt.clf()

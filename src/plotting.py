399920d0bd834d53f0b8fd1caeb9d3057b4d2144
Shankara <shankaravandeven@hotmail.com>
2022-02-11 11:24:43 +0100

    Provide color options in bodylines plotting

--- src/plotting.py
+++ src/plotting.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-BODYPART_LINES = [('ankle1', 'knee1', 'ro-'), 
-                  ('knee1', 'hip1', 'go-'),
-                  ('hip1', 'shoulder1', 'yo-'),
-                  ('shoulder1', 'elbow1', 'co-'),
-                  ('elbow1', 'wrist1', 'mo-'),
-                  ('ankle2', 'knee2', 'ro-'), 
-                  ('knee2', 'hip2', 'go-'),
-                  ('hip2', 'shoulder2', 'yo-'),
-                  ('shoulder2', 'elbow2', 'co-'),
-                  ('elbow2', 'wrist2', 'mo-'),
-                  ('hip1', 'hip2', 'yo-'),
-                  ('shoulder1', 'shoulder2', 'co-')]
+BODYPART_LINES = [('ankle1', 'knee1'), 
+                  ('knee1', 'hip1'),
+                  ('hip1', 'shoulder1'),
+                  ('shoulder1', 'elbow1'),
+                  ('elbow1', 'wrist1'),
+                  ('ankle2', 'knee2'), 
+                  ('knee2', 'hip2'),
+                  ('hip2', 'shoulder2'),
+                  ('shoulder2', 'elbow2'),
+                  ('elbow2', 'wrist2'),
+                  ('hip1', 'hip2'),
+                  ('shoulder1', 'shoulder2')]
+BODYPART_LINES_COLORS = ['ro-', 'go-', 'yo-', 'co-', 'mo-', 'ro-', 'go-', 'yo-', 'co-', 'mo-', 'yo-', 'co-']
 DEFAULT_BODYPARTS = ['ankle1', 'knee1', 'hip1', 'shoulder1', 'elbow1', 'wrist1', 'ankle2', 'knee2', 'hip2', 'shoulder2', 'elbow2', 'wrist2']
 COLORS = ['ro-', 'go-', 'yo-', 'co-', 'mo-', 'bo-']
 
@@ -42,8 +43,8 @@ def plot_continues_line(df, bodypart1, bodypart2, frame=None):
         y2 = df[bodypart2]['y'][frame]
     continues_line((x1, y1), (x2, y2))
 
-def plot_bodypart_lines(df, frame = None, bodypart_lines = BODYPART_LINES):
-    for line in bodypart_lines:
+def plot_bodypart_lines(df, frame = None, bodypart_lines = BODYPART_LINES, bodypart_lines_colors = BODYPART_LINES_COLORS):
+    for line, color in zip(bodypart_lines, bodypart_lines_colors):
         if frame == None:
             x1 = df[line[0]]['x'].mean()
             y1 = df[line[0]]['y'].mean()
@@ -54,7 +55,7 @@ def plot_bodypart_lines(df, frame = None, bodypart_lines = BODYPART_LINES):
             y1 = df[line[0]]['y'][frame]
             x2 = df[line[1]]['x'][frame]
             y2 = df[line[1]]['y'][frame]
-        plt.plot([x1, x2], [y1, y2], line[2])
+        plt.plot([x1, x2], [y1, y2], color)
     
 def plot_bodypoint(df, bodypart, color, alpha = 0.1, frame = None):
     if frame == None:

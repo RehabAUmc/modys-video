9bf8255349d7c0dcda27baa34f0ea19f0d3402ac
Helga Haberfehlner <54277291+hhr210@users.noreply.github.com>
2022-03-23 23:14:54 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 PROJECT_ROOT_FOLDER = Path(__file__).parent.parent
 DATA_FOLDER = PROJECT_ROOT_FOLDER / 'data'
-SCORES_DATA_FILEPATH = DATA_FOLDER / 'data_Scoring_DIS_proximal_trunk_V1.1.xlsx'
-LYING_VIDEOS_DATA_FOLDER = DATA_FOLDER / 'data_lying_052929'
-SITTING_VIDEOS_DATA_FOLDER = DATA_FOLDER / 'data_sitting_052020'
+SCORES_DATA_FILEPATH = DATA_FOLDER / 'data_clinical_scoring.xlsx'
+LYING_VIDEOS_DATA_FOLDER = DATA_FOLDER / 'data_stickfigure_coordinates_lying'
+SITTING_VIDEOS_DATA_FOLDER = DATA_FOLDER / 'data_stickfigure_coordinates_sitting'
